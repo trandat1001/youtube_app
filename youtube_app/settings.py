56424ce53856dc37ded3e8cc0a71f97f3b6fdada
Dat Tran <trandat100185@gmail.com>
2020-03-12 17:52:45 +0700

    Click to icon redirect to homepage

--- youtube_app/settings.py
+++ youtube_app/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'view_youtube.urls'
+ROOT_URLCONF = 'youtube_app.urls'
 
 TEMPLATES = [
     {
@@ -70,7 +70,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'view_youtube.wsgi.application'
+WSGI_APPLICATION = 'youtube_app.wsgi.application'
 
 
 # Database
